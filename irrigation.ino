e51044bcf835b31d36a37f0788bc94a15a4f88f1
Max Frei <max.frei@uni-due.de>
2020-04-12 18:49:31 +0200

    Refactoring.

--- irrigation.ino
+++ irrigation.ino
@@ -1,58 +1,105 @@
+// Includes -------------------------------------------------------------------------------------
 #include <LiquidCrystal.h>
+#include <Encoder.h>
 
-// initialize the library with the numbers of the interface pins
 
-const byte pinLcdRs = 2;
-const byte pinLcdE = 3;
+// Constants and global variables ---------------------------------------------------------------
+
+// LCD
+const byte pinLcdRs = 11;
+const byte pinLcdE = 10;
 const byte pinLcdD4 = 4;
 const byte pinLcdD5 = 5;
 const byte pinLcdD6 = 6;
 const byte pinLcdD7 = 7;
+
+const byte numLcdRows = 2;
+const byte numLcdColumns = 16;
 LiquidCrystal lcd(pinLcdRs, pinLcdE, pinLcdD4, pinLcdD5, pinLcdD6, pinLcdD7);
 
+// Section control
 const byte numIrrigationSections = 3; // Number of irrigation sections
 byte currentIrrigationSection = 0;
-
 const byte pinSectionSwitch = 9;
-byte stateSectionSwitch = 0; 
+byte stateSectionSwitch = 0;
 
+// Override button
 const byte pinOverrideSwitch = 8;
-byte stateOverrideSwitch = 0; 
+byte stateOverrideSwitch = 0;
 bool isOverrideActive = false;
 
-const byte pinLed = 13;
+// Target value control
+int irrigationSectionTargets[numIrrigationSections];
+const byte pinTargetValueRotaryEncoderDt = 3;
+const byte pinTargetValueRotaryEncoderClk = 2;
+Encoder targetValueRotaryEncoder(pinTargetValueRotaryEncoderClk, pinTargetValueRotaryEncoderDt);
+long posTargetValueRotaryEncoderOld = -999;
+long posTargetValueRotaryEncoderNew = -999;
 
+// Miscellaneous variables and constants
+const byte pinLed = 13;
 const int delayButtons = 250;
 
-void setup(){
-  // set up the LCD’s number of columns and rows:
-  lcd.begin(16, 2);
-  
-  // initialize the pushbutton pin as an input:
+// Setup routine -------------------------------------------------------------------------------
+void setup() {
+  lcd.begin(numLcdColumns, numLcdRows);
+
   pinMode(pinSectionSwitch, INPUT_PULLUP);
 
   updateDisplay();
 }
 
- 
 
-void loop(){
-  if (!isOverrideActive){
-    stateSectionSwitch = !digitalRead(pinSectionSwitch);
-    if (stateSectionSwitch){
-      switchIrrigationSection();
-    }
+// Main loop -----------------------------------------------------------------------------------
+void loop() {
+  if (!isOverrideActive) {
+    checkSectionSwitch();
+    updateTargetValueRotaryEncoder();
   }
 
-  stateOverrideSwitch = digitalRead(pinOverrideSwitch);
-  if (stateOverrideSwitch == HIGH){
-    toggleOverride();
-  }
-  
-
+  checkOverrideSwitch();
 }
 
-void switchLed(bool state){
+void checkOverrideSwitch(){
+  stateOverrideSwitch = digitalRead(pinOverrideSwitch);
+  if (stateOverrideSwitch == HIGH) {
+    toggleOverride();
+  }
+}
+
+void checkSectionSwitch(){
+  stateSectionSwitch = !digitalRead(pinSectionSwitch);
+  if (stateSectionSwitch) {
+    switchIrrigationSection();
+  }
+}
+
+void updateTargetValueRotaryEncoder()
+{
+  posTargetValueRotaryEncoderNew = targetValueRotaryEncoder.read();
+
+  if (posTargetValueRotaryEncoderNew != posTargetValueRotaryEncoderOld) {
+    if (posTargetValueRotaryEncoderNew > posTargetValueRotaryEncoderOld) {
+      irrigationSectionTargets[currentIrrigationSection]++;
+    }
+    else {
+      irrigationSectionTargets[currentIrrigationSection]--;
+    }
+
+    if (irrigationSectionTargets[currentIrrigationSection] > 100) {
+      irrigationSectionTargets[currentIrrigationSection] = 100;
+    }
+
+    if (irrigationSectionTargets[currentIrrigationSection] < 0) {
+      irrigationSectionTargets[currentIrrigationSection] = 0;
+    }
+
+    posTargetValueRotaryEncoderOld = posTargetValueRotaryEncoderNew;
+    updateDisplay();
+  }
+}
+
+void switchLed(bool state) {
   if (state) {
     digitalWrite(pinLed, HIGH);
   } else {
@@ -60,32 +107,33 @@ void switchLed(bool state){
   }
 }
 
-void switchIrrigationSection(){
+void switchIrrigationSection() {
   currentIrrigationSection++;
-  currentIrrigationSection%=numIrrigationSections;
+  currentIrrigationSection %= numIrrigationSections;
   updateDisplay();
   delay(delayButtons);
 }
 
-void toggleOverride(){
+void toggleOverride() {
   isOverrideActive = !isOverrideActive;
   switchLed(isOverrideActive);
   delay(delayButtons);
   updateDisplay();
 }
 
-void updateDisplay(){
+void updateDisplay() {
   lcd.clear();
   String displayRow1 = String("");
   String displayRow2 = String("");
 
-  if (isOverrideActive){
-    displayRow1 = String("OVERRIDE ACTIVE!");
+  if (isOverrideActive) {
+    displayRow1 = String("!!!TESTMODUS!!!");
   } else {
-    displayRow1 = String("Section ") + String(currentIrrigationSection+1);
+    displayRow1 = String("Sektion ") + String(currentIrrigationSection + 1);
+    displayRow2 = String("Soll: ") + String(irrigationSectionTargets[currentIrrigationSection]);
   }
-  
-  
+
+
   lcd.setCursor(0, 0);
   lcd.print(displayRow1);
   lcd.setCursor(0, 1);
